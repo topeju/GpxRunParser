6d3af3d0c5c53826dccb517dc671efdb4e4b389c
Tomi Junnila <topeju@badzilla.net>
2014-06-11 00:54:15 +0300

    Initial version: Processes a single GPX file for binning by heart rate and pace

---
+++ GpxRunParser/GpxTrackPoint.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace GpxRunParser
+{
+	public class GpxTrackPoint
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+		public double Elevation { get; set; }
+		public DateTime Time { get; set; }
+		public double HeartRate { get; set; }
+		public double Cadence { get; set; }
+
+		public GpxTrackPoint()
+		{
+		}
+
+		public GpxTrackPoint(XElement element)
+			: this()
+		{
+			var gpxNamespace = XNamespace.Get("http://www.topografix.com/GPX/1/1");
+			var gpxTrackPointNamespace = XNamespace.Get("http://www.garmin.com/xmlschemas/TrackPointExtension/v1");
+			Latitude = double.Parse(element.Attribute("lat").Value, CultureInfo.InvariantCulture);
+			Longitude = double.Parse(element.Attribute("lon").Value, CultureInfo.InvariantCulture);
+			var elevElem = element.Element(gpxNamespace + "ele");
+			if (elevElem != null) Elevation = double.Parse(elevElem.Value, CultureInfo.InvariantCulture);
+			var timeElem = element.Element(gpxNamespace + "time");
+			if (timeElem != null) Time = DateTime.Parse(timeElem.Value, CultureInfo.InvariantCulture);
+			var extElem = element.Element(gpxNamespace + "extensions");
+			if (extElem != null) {
+				var trackPointExtElem = extElem.Element(gpxTrackPointNamespace + "TrackPointExtension");
+				if (trackPointExtElem != null) {
+					var hrElem = trackPointExtElem.Element(gpxTrackPointNamespace + "hr");
+					if (hrElem != null) HeartRate = double.Parse(hrElem.Value, CultureInfo.InvariantCulture);
+					var cadenceElem = trackPointExtElem.Element(gpxTrackPointNamespace + "cad");
+					if (cadenceElem != null) Cadence = double.Parse(cadenceElem.Value, CultureInfo.InvariantCulture);
+				}
+			}
+		}
+
+		public double DistanceTo(GpxTrackPoint otherPoint)
+		{
+			const double equatorialRadius = 6378137.0D; // m
+			var latDiff = (otherPoint.Latitude - this.Latitude) * Math.PI / 180.0;
+			var lonDiff = (otherPoint.Longitude - this.Longitude) * Math.PI / 180.0;
+			var lat1 = this.Latitude * Math.PI / 180.0;
+			var lat2 = otherPoint.Latitude * Math.PI / 180.0;
+			var a = Math.Sin(latDiff / 2.0) * Math.Sin(latDiff / 2.0)
+					+ Math.Sin(lonDiff / 2.0) * Math.Sin(lonDiff / 2.0) * Math.Cos(lat1) * Math.Cos(lat2);
+			var c = 2.0 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1.0 - a));
+			return equatorialRadius * c;
+		}
+
+		public TimeSpan TimeDifference(GpxTrackPoint otherPoint)
+		{
+			return otherPoint.Time - this.Time;
+		}
+	}
+}
---
+++ GpxRunParser/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using NDesk.Options;
+
+namespace GpxRunParser
+{
+	public static class Program
+	{
+		private static void Main(string[] args)
+		{
+			var fileName = @"D:\Data\Dropbox\Apps\iSmoothRun\Export\2014-06-09T17_26_45+300_Running.gpx";
+			var zoneStr = "120,140,173,182";
+			var paceBinStr = "8:45,7:15,6:10";
+			var opts = new OptionSet {
+				{ "f|file=", "GPX file to parse", s => fileName = s },
+				{ "z|zones=", "Heart rate zone limits, comma-separated", s => zoneStr = s },
+				{ "p|paces=", "Pace bin limits, comma-separated mm:ss", s => paceBinStr = s }
+			};
+			try {
+				opts.Parse(args);
+			}
+			catch (OptionException e) {
+				Console.Error.Write("GpxRunParser: ");
+				Console.Error.WriteLine(e.Message);
+				Console.Error.WriteLine("Try GpxRunParser --help for more information");
+				return;
+			}
+			if (fileName == "") throw new OptionException("file", "The file parameter must be specified");
+			var zones = (from zone in zoneStr.Split(',') select double.Parse(zone, CultureInfo.InvariantCulture)).ToArray();
+			var paces =
+				(from paceStr in paceBinStr.Split(',')
+				 select new TimeSpan(0, int.Parse(paceStr.Split(':')[0]), int.Parse(paceStr.Split(':')[1])).TotalMinutes).ToArray();
+
+			var gpxNamespace = XNamespace.Get("http://www.topografix.com/GPX/1/1");
+			var gpxDoc = XDocument.Load(fileName);
+
+			var zoneBins = new TimeBin(zones);
+			var paceBins = new TimeBin(paces);
+			var totalTime = TimeSpan.Zero;
+			var totalDistance = 0.0D;
+
+			foreach (var track in gpxDoc.Descendants(gpxNamespace + "trk")) {
+				foreach (var segment in track.Descendants(gpxNamespace + "trkseg")) {
+					var points = from point in segment.Descendants(gpxNamespace + "trkpt")
+								 select new GpxTrackPoint(point);
+					var iterator = points.GetEnumerator();
+					if (iterator.MoveNext()) {
+						var p0 = iterator.Current;
+						while (iterator.MoveNext()) {
+							var pt = iterator.Current;
+							var dist = p0.DistanceTo(pt);
+							totalDistance += dist;
+							var deltaT = p0.TimeDifference(pt);
+							totalTime += deltaT;
+							var pace = deltaT.TotalMinutes * 1000.0D / dist;
+							zoneBins.Record(p0.HeartRate, deltaT);
+							paceBins.Record(pace, deltaT);
+							p0 = pt;
+						}
+					}
+				}
+			}
+
+			var extRegexp = new Regex(@"\.gpx$", RegexOptions.IgnoreCase);
+			var outputFileName = extRegexp.Replace(fileName, ".txt");
+
+			using (var output = File.CreateText(outputFileName)) {
+				output.WriteLine("Total distance:\t{0:N2} km", totalDistance/1000.0D);
+				output.WriteLine("Total time:\t{0:g}", totalTime);
+				output.WriteLine();
+
+				output.WriteLine("Time spent in each heart rate zone:");
+				zoneBins.Output(output, totalTime);
+
+				output.WriteLine();
+				output.WriteLine("Time spent in each pace range:");
+				paceBins.Output(output, totalTime);
+			}
+
+		}
+	}
+
+	public class TimeBin
+	{
+		private double[] Bins { get; set; }
+
+		private TimeSpan[] Values { get; set; }
+
+		public TimeBin(double[] boundaries)
+		{
+			Bins = new double[boundaries.Length];
+			Values = new TimeSpan[boundaries.Length + 1];
+			var i = 0;
+			Values[0] = TimeSpan.Zero;
+			foreach (var b in boundaries.OrderBy(bnd => bnd)) {
+				Bins[i] = b;
+				Values[i+1] = TimeSpan.Zero;
+				i++;
+			}
+		}
+
+		public void Record(double value, TimeSpan time)
+		{
+			var i = Bins.Length-1;
+			while (i >= 0 && Bins[i] > value) i--;
+			Values[i+1] += time;
+		}
+
+		public void Output(StreamWriter stream, TimeSpan totalTime)
+		{
+			stream.WriteLine("{0,-20}\t{1:g}\t{2:P0}", "< " + Bins[0], Values[0],
+								  Values[0].TotalMilliseconds / totalTime.TotalMilliseconds);
+			for (var i = 0; i < Bins.Length - 1; i++) {
+				stream.WriteLine("{0,-20}\t{1:g}\t{2:P0}", Bins[i] + "..." + Bins[i + 1], Values[i + 1],
+									  Values[i+1].TotalMilliseconds / totalTime.TotalMilliseconds);
+			}
+			stream.WriteLine("{0,-20}\t{1:g}\t{2:P0}", "> " + Bins[Bins.Length - 1], Values[Bins.Length],
+								  Values[Bins.Length].TotalMilliseconds / totalTime.TotalMilliseconds);
+		}
+	}
+}
