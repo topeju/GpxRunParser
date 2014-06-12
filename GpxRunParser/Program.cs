871090dcf90ac3d8fa6ed6732041861ee28d1068
Tomi Junnila <topeju@badzilla.net>
2014-06-12 19:32:08 +0300

    Use the Razor view engine to generate HTML reports

--- GpxRunParser/Program.cs
+++ GpxRunParser/Program.cs
@@ -2,9 +2,11 @@
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using NDesk.Options;
+using RazorEngine;
 
 namespace GpxRunParser
 {
@@ -24,8 +26,7 @@ namespace GpxRunParser
 			};
 			try {
 				opts.Parse(args);
-			}
-			catch (OptionException e) {
+			} catch (OptionException e) {
 				Console.Error.Write("GpxRunParser: ");
 				Console.Error.WriteLine(e.Message);
 				Console.Error.WriteLine("Try GpxRunParser --help for more information");
@@ -41,7 +42,8 @@ namespace GpxRunParser
 				Console.Error.WriteLine("Input GPX file not specified");
 				return;
 			}
-			var zones = (from zone in zoneStr.Split(',') select double.Parse(zone, CultureInfo.InvariantCulture)).ToArray();
+			var zones = (from zone in zoneStr.Split(',')
+			             select double.Parse(zone, CultureInfo.InvariantCulture)).ToArray();
 			var paces =
 				(from paceStr in paceBinStr.Split(',')
 				 select new TimeSpan(0, int.Parse(paceStr.Split(':')[0]), int.Parse(paceStr.Split(':')[1]))).ToArray();
@@ -49,38 +51,21 @@ namespace GpxRunParser
 			var gpxNamespace = XNamespace.Get("http://www.topografix.com/GPX/1/1");
 			var gpxDoc = XDocument.Load(fileName);
 
-			var zoneBins = new TimeBin<double>(zones);
-			var paceBins = new TimeBin<TimeSpan>(paces);
-			var totalTime = TimeSpan.Zero;
-			var totalDistance = 0.0D;
-			var heartTotal = 0.0D;
-			var maxHeartRate = 0.0D;
-			var totalSteps = 0.0D;
+			var runStats = new RunStats(zones, paces);
 
 			foreach (var track in gpxDoc.Descendants(gpxNamespace + "trk")) {
 				foreach (var segment in track.Descendants(gpxNamespace + "trkseg")) {
 					var points = from point in segment.Descendants(gpxNamespace + "trkpt")
-								 select new GpxTrackPoint(point);
+					             select new GpxTrackPoint(point);
 					var iterator = points.GetEnumerator();
 					if (iterator.MoveNext()) {
 						var p0 = iterator.Current;
-						if (p0.HeartRate > maxHeartRate) {
-							maxHeartRate = p0.HeartRate;
-						}
+						runStats.UpdateMaxHeartRate(p0.HeartRate);
 						while (iterator.MoveNext()) {
 							var pt = iterator.Current;
 							var dist = p0.DistanceTo(pt);
-							totalDistance += dist;
 							var deltaT = p0.TimeDifference(pt);
-							zoneBins.Record(p0.HeartRate, deltaT);
-							totalTime += deltaT;
-							var pace = new TimeSpan((long)(1000.0D * deltaT.Ticks / dist));
-							paceBins.Record(pace, deltaT);
-							heartTotal += p0.HeartRate * deltaT.TotalMinutes;
-							if (pt.HeartRate > maxHeartRate) {
-								maxHeartRate = pt.HeartRate;
-							}
-							totalSteps += 2.0D * p0.Cadence * deltaT.TotalMinutes; // Cadence is number of full cycles per minute by the pair of feet, thus there are two steps per cadence per minute?
+							runStats.RecordInterval(deltaT, dist, pt.HeartRate, pt.Cadence);
 							p0 = pt;
 						}
 					}
@@ -88,10 +73,18 @@ namespace GpxRunParser
 			}
 
 			var extRegexp = new Regex(@"\.gpx$", RegexOptions.IgnoreCase);
-			var outputFileName = extRegexp.Replace(fileName, ".txt");
+			var outputFileName = extRegexp.Replace(fileName, ".html");
+
+			var assembly = Assembly.GetExecutingAssembly();
+
+			using (var stream = assembly.GetManifestResourceStream("GpxRunParser.Templates.index.cshtml")) {
+				using (var reader = new StreamReader(stream)) {
+					var page = Razor.Parse(reader.ReadToEnd(), runStats);
+				}
+			}
 
 			using (var output = File.CreateText(outputFileName)) {
-				output.WriteLine("{0,-20}  {1:N2} km", "Total distance:", totalDistance/1000.0D);
+				output.WriteLine("{0,-20}  {1:N2} km", "Total distance:", totalDistance / 1000.0D);
 				output.WriteLine("{0,-20}  {1:g}", "Total time:", totalTime);
 				output.WriteLine("{0,-20}  {1:g}/km", "Average pace:", new TimeSpan((long)(1000.0D * totalTime.Ticks / totalDistance)));
 				output.WriteLine("{0,-20}  {1:N2} km/h", "Average speed:", totalDistance / (1000.0D * totalTime.TotalHours));
@@ -111,5 +104,64 @@ namespace GpxRunParser
 			}
 
 		}
+	}
+
+	public class RunStats
+	{
+		public double TotalDistance { get; private set; }
+
+		public double TotalDistanceInKm { get { return TotalDistance / 1000.0D; } }
+
+		public TimeSpan TotalTime { get; private set; }
+
+		public TimeSpan AveragePace { get { return new TimeSpan((long)(1000.0D * TotalTime.Ticks / TotalDistanceInKm)); } }
+
+		public double AverageSpeed { get { return TotalDistanceInKm / TotalTime.TotalHours; } }
+
+		public double TotalHeartbeats { get; private set; }
+
+		public double AverageHeartRate { get { return TotalHeartbeats / TotalTime.TotalMinutes; } }
+
+		public double MaxHeartRate { get; private set; }
+
+		public double TotalSteps { get; private set; }
+
+		public double AverageCadence { get { return TotalSteps / (2.0D * TotalTime.TotalMinutes); } }
+
+		public double AverageStrideLength { get { return TotalDistanceInKm * 100.0D / TotalSteps; } }
+
+		private TimeBin<double> _zoneBins;
+		private TimeBin<TimeSpan> _paceBins;
+
+		public RunStats(double[] zones, TimeSpan[] paces)
+		{
+			_zoneBins = new TimeBin<double>(zones);
+			_paceBins = new TimeBin<TimeSpan>(paces);
+			TotalDistance = 0.0D;
+			TotalTime = TimeSpan.Zero;
+			TotalHeartbeats = 0.0D;
+			MaxHeartRate = 0.0D;
+			TotalSteps = 0.0D;
+		}
+
+		public void RecordInterval(TimeSpan duration, double distance, double heartRate, double cadence)
+		{
+			TotalTime += duration;
+			TotalDistance += distance;
+			_zoneBins.Record(heartRate, duration);
+			var pace = new TimeSpan((long)(1000.0D * duration.Ticks / distance));
+			_paceBins.Record(pace, duration);
+			TotalHeartbeats += heartRate * duration.TotalMinutes;
+			UpdateMaxHeartRate(heartRate);
+			// Cadence is number of full cycles per minute by the pair of feet, thus there are two steps per cadence per minute?
+			TotalSteps += 2.0D * cadence * duration.TotalMinutes;
+		}
+
+		public void UpdateMaxHeartRate(double heartRate)
+		{
+			if (heartRate > MaxHeartRate)
+				MaxHeartRate = heartRate;
+		}
+		
 	}
 }
