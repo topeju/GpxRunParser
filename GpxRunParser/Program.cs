fca1a56f9e9da7c30d952f9f265ede161ff76cb0
Tomi Junnila <topeju@badzilla.net>
2015-07-13 10:41:21 +0300

    Updated RazorEngine to the latest version, with compiled template caching

--- GpxRunParser/Program.cs
+++ GpxRunParser/Program.cs
@@ -7,7 +7,6 @@ using System.Reflection;
 using System.Text.RegularExpressions;
 using GpxRunParser.Charts.Distance;
 using GpxRunParser.Charts.Time;
-using NDesk.Options;
 using RazorEngine;
 using RazorEngine.Templating;
 
@@ -17,30 +16,8 @@ namespace GpxRunParser
 	{
 		private static void Main(string[] args)
 		{
-			var dirName = "";
-			var help = false;
-			var opts = new OptionSet {
-				{ "d|dir=", "Directory with GPX files to parse", s => dirName = s },
-				{ "h|?|help", "Show this help text", s => help = s != null }
-			};
-			try {
-				opts.Parse(args);
-			}
-			catch (OptionException e) {
-				Console.Error.Write("GpxRunParser: ");
-				Console.Error.WriteLine(e.Message);
-				Console.Error.WriteLine("Try GpxRunParser --help for more information");
-				return;
-			}
-			if (help) {
-				Console.Out.WriteLine("Usage: GpxRunParser [OPTIONS]+");
-				Console.Out.WriteLine("Parses the GPX files in the directory specified using the -d option to");
-				Console.Out.WriteLine("calculate statistics on the files.");
-				opts.WriteOptionDescriptions(Console.Out);
-				return;
-			}
-			if (dirName == "") {
-				Console.Error.WriteLine("Input directory not specified");
+			if (args.Length == 0) {
+				Console.Error.WriteLine("Input directory/directories not specified");
 				return;
 			}
 
@@ -48,6 +25,9 @@ namespace GpxRunParser
 
 			var assembly = Assembly.GetExecutingAssembly();
 			var pageTemplate = "";
+			bool individualRunCompiled = false;
+			bool monthlyCompiled = false;
+			bool weeklyCompiled = false;
 
 			using (var stream = assembly.GetManifestResourceStream("GpxRunParser.Templates.IndividualRun.cshtml")) {
 				using (var reader = new StreamReader(stream)) {
@@ -56,74 +36,93 @@ namespace GpxRunParser
 			}
 
 			var extRegexp = new Regex(@"\.gpx$", RegexOptions.IgnoreCase);
-			var gpxFiles = Directory.EnumerateFiles(dirName, Settings.FilePattern);
 
 			var runs = new List<RunInfo>();
 
-			foreach (var fileName in gpxFiles) {
-				var runStats = analyzer.Analyze(fileName);
+			foreach (var dirName in args) {
+				var gpxFiles = Directory.EnumerateFiles(dirName, Settings.FilePattern);
 
-				var baseFileName = extRegexp.Replace(fileName, "");
-				var outputFileName = baseFileName + ".html";
-				var viewBag = new DynamicViewBag();
-				viewBag.AddValue("FileName", baseFileName);
-				var page = Razor.Parse(pageTemplate, runStats, viewBag, null);
-				using (var output = File.CreateText(outputFileName)) {
-					output.Write(page);
+				foreach (var fileName in gpxFiles) {
+					var runStats = analyzer.Analyze(fileName);
+
+					var baseFileName = extRegexp.Replace(fileName, "");
+					var outputFileName = baseFileName + ".html";
+					var viewBag = new DynamicViewBag();
+					viewBag.AddValue("FileName", baseFileName);
+					string page;
+					if (!individualRunCompiled) {
+						page = Engine.Razor.RunCompile(pageTemplate, "IndividualRun", typeof(RunStatistics), runStats, viewBag);
+						individualRunCompiled = true;
+					} else {
+						page = Engine.Razor.Run("IndividualRun", typeof(RunStatistics), runStats, viewBag);
+					}
+					using (var output = File.CreateText(outputFileName)) {
+						output.Write(page);
+					}
+
+					runs.Add(new RunInfo(outputFileName, runStats));
+
+					var hrChart = new HeartRateTimeChart(baseFileName, runStats);
+					hrChart.Draw();
+					hrChart.SavePng();
+
+					var paceChart = new PaceTimeChart(baseFileName, runStats);
+					paceChart.Draw();
+					paceChart.SavePng();
+
+					var cadenceChart = new CadenceTimeChart(baseFileName, runStats);
+					cadenceChart.Draw();
+					cadenceChart.SavePng();
+
+					var elevationChart = new ElevationTimeChart(baseFileName, runStats);
+					elevationChart.Draw();
+					elevationChart.SavePng();
+
+					var hrDistChart = new HeartRateDistanceChart(baseFileName, runStats);
+					hrDistChart.Draw();
+					hrDistChart.SavePng();
+
+					var paceDistChart = new PaceDistanceChart(baseFileName, runStats);
+					paceDistChart.Draw();
+					paceDistChart.SavePng();
+
+					var cadenceDistChart = new CadenceDistanceChart(baseFileName, runStats);
+					cadenceDistChart.Draw();
+					cadenceDistChart.SavePng();
+
+					var elevationDistChart = new ElevationDistanceChart(baseFileName, runStats);
+					elevationDistChart.Draw();
+					elevationDistChart.SavePng();
 				}
-
-				runs.Add(new RunInfo(outputFileName, runStats));
-
-				var hrChart = new HeartRateTimeChart(baseFileName, runStats);
-				hrChart.Draw();
-				hrChart.SavePng();
-
-				var paceChart = new PaceTimeChart(baseFileName, runStats);
-				paceChart.Draw();
-				paceChart.SavePng();
-
-				var cadenceChart = new CadenceTimeChart(baseFileName, runStats);
-				cadenceChart.Draw();
-				cadenceChart.SavePng();
-
-				var elevationChart = new ElevationTimeChart(baseFileName, runStats);
-				elevationChart.Draw();
-				elevationChart.SavePng();
-
-				var hrDistChart = new HeartRateDistanceChart(baseFileName, runStats);
-				hrDistChart.Draw();
-				hrDistChart.SavePng();
-
-				var paceDistChart = new PaceDistanceChart(baseFileName, runStats);
-				paceDistChart.Draw();
-				paceDistChart.SavePng();
-
-				var cadenceDistChart = new CadenceDistanceChart(baseFileName, runStats);
-				cadenceDistChart.Draw();
-				cadenceDistChart.SavePng();
-
-				var elevationDistChart = new ElevationDistanceChart(baseFileName, runStats);
-				elevationDistChart.Draw();
-				elevationDistChart.SavePng();
 			}
 
-			using (var stream = assembly.GetManifestResourceStream("GpxRunParser.Templates.MonthlyStatistics.cshtml")) {
-				using (var reader = new StreamReader(stream)) {
-					pageTemplate = reader.ReadToEnd();
+			if (!monthlyCompiled) {
+				using (var stream = assembly.GetManifestResourceStream("GpxRunParser.Templates.MonthlyStatistics.cshtml")) {
+					using (var reader = new StreamReader(stream)) {
+						pageTemplate = reader.ReadToEnd();
+					}
 				}
 			}
 
 			foreach (var month in analyzer.MonthlyStats.Keys) {
 				var outputFileName = String.Format("Monthly-{0:yyyy-MM}.html", month);
-				var page = Razor.Parse(pageTemplate, analyzer.MonthlyStats[month]);
+				string page;
+				if (!monthlyCompiled) {
+					page = Engine.Razor.RunCompile(pageTemplate, "MonthlyStatistics", typeof(RunStatistics), analyzer.MonthlyStats[month]);
+					monthlyCompiled = true;
+				} else {
+					page = Engine.Razor.Run("MonthlyStatistics", typeof(RunStatistics), analyzer.MonthlyStats[month]);
+				}
 				using (var output = File.CreateText(outputFileName)) {
 					output.Write(page);
 				}
 			}
 
-			using (var stream = assembly.GetManifestResourceStream("GpxRunParser.Templates.WeeklyStatistics.cshtml")) {
-				using (var reader = new StreamReader(stream)) {
-					pageTemplate = reader.ReadToEnd();
+			if (!weeklyCompiled) {
+				using (var stream = assembly.GetManifestResourceStream("GpxRunParser.Templates.WeeklyStatistics.cshtml")) {
+					using (var reader = new StreamReader(stream)) {
+						pageTemplate = reader.ReadToEnd();
+					}
 				}
 			}
 
@@ -132,7 +131,13 @@ namespace GpxRunParser
 				var outputFileName = String.Format("Weekly-{0:yyyy}-{1:D2}.html",
 												   week,
 												   calendar.GetWeekOfYear(week, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday));
-				var page = Razor.Parse(pageTemplate, analyzer.WeeklyStats[week]);
+				string page;
+				if (!weeklyCompiled) {
+					page = Engine.Razor.RunCompile(pageTemplate, "WeeklyStatistics", typeof(RunStatistics), analyzer.WeeklyStats[week]);
+					weeklyCompiled = true;
+				} else {
+					page = Engine.Razor.Run("WeeklyStatistics", typeof(RunStatistics), analyzer.WeeklyStats[week]);
+				}
 				using (var output = File.CreateText(outputFileName)) {
 					output.Write(page);
 				}
@@ -152,7 +157,7 @@ namespace GpxRunParser
 				endDate = new DateTime(endDate.Year, endDate.Month, 1).AddMonths(1).AddDays(-1);
 				indexViewBag.AddValue("StartDate", startDate);
 				indexViewBag.AddValue("EndDate", endDate);
-				var indexPage = Razor.Parse(pageTemplate, runs, indexViewBag, null);
+				var indexPage = Engine.Razor.RunCompile(pageTemplate, "Index", typeof(IList<RunInfo>), runs, indexViewBag);
 				using (var output = File.CreateText("Index.html")) {
 					output.Write(indexPage);
 				}
--- GpxRunParser/Templates/ModelTemplate.cs
+++
@@ -1,9 +0,0 @@
-﻿using RazorEngine.Templating;
-
-namespace GpxRunParser.Templates
-{
-	public class ModelTemplate<T> : TemplateBase<T>
-	{
-		public new T Model { get; set; }
-	}
-}
